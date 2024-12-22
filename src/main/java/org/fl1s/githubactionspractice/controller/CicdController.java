b1dc3d797a769f1296724d1d850b274a72938ba2
Fl1s <maksima7th@gmail.com>
2024-12-22 14:45:29 +0700

    fix

--- src/main/java/org/fl1s/githubactionspractice/controller/CicdController.java
+++ src/main/java/org/fl1s/githubactionspractice/controller/CicdController.java
@@ -1,4 +1,4 @@
-package org.fl1s.controller;
+package org.fl1s.githubactionspractice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/test/java/org/fl1s/githubactionspractice/GithubActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fl1s.githubactionspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
