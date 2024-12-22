c5b4d2c5b62f885c48a876022a1c4c28b057677a
Fl1s <maksima7th@gmail.com>
2024-12-22 12:05:10 +0700

    Fix
    saas
    
    again
    
    Update gradle.yml
    
    dsa
    
    test
    
    refactor

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:21
 EXPOSE 8080
-ADD  target/spring-images-new.jar spring-images-new.jar
+ADD  build/libs/spring-images-new.jar spring-images-new.jar
 ENTRYPOINT ["java", "-jar", "/spring-images-new.jar"]
---
+++ src/main/java/org/fl1s/GithubCicdActionsApplication.java
@@ -0,0 +1,11 @@
+package org.fl1s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+    public class GithubCicdActionsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+}
