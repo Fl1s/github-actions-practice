024479a58be64b9c8eebe29b6ef6c625b3c06c9d
Fl1s <maksima7th@gmail.com>
2024-12-22 15:25:13 +0700

    fix

--- src/main/java/org/fl1s/githubactionspractice/GithubActionsPracticeApplication.java
+++ src/main/java/org/fl1s/githubactionspractice/GithubActionsPracticeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubActionsPracticeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsPracticeApplication.class, args);
 	}
-
 }
